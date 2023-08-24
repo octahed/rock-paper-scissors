153954da62f4c9c49fbd915e513e07df601f72e5
octahed <142986153+octahed@users.noreply.github.com>
2023-08-24 17:02:39 +0300

    Add method that plays a round of the game

--- rps.rb
+++ rps.rb
@@ -4,11 +4,30 @@ def get_computer_choice
   CHOICES.sample
 end
 
-#Create a method that gets a choice from the user
+# Create a method that gets a choice from the user
 def get_player_choice 
   puts "Enter a number that corresponds with your choice"
   CHOICES.each_index {|i| puts "#{i + 1}: #{CHOICES[i]}"}
   print "> "
   answer = gets.chomp.to_i
   CHOICES[answer - 1]
+end
+
+# Gets player's and computer's choices and declares the winner
+def play_round (players_choice, computers_choice)
+  if players_choice == computers_choice 
+    puts "Tie"
+  elsif players_choice == "rock" && computers_choice == "paper"
+    puts "Paper beats rock, you lose!"
+  elsif players_choice == "rock" && computers_choice == "scissors"
+    puts "Rock beats scissors, you win!"
+  elsif players_choice == "paper" && computers_choice == "rock"
+    puts "Paper beats rock, you win!"
+  elsif players_choice == "paper" && computers_choice == "scissors"
+    puts "Scissors beat paper, you lose!"
+  elsif players_choice == "scissors" && computers_choice == "paper"
+    puts "Scissors beat paper, you win"
+  elsif players_choice == "scissors" && computers_choice == "rock"
+    puts "Rock beats scissors, you lose!"
+  end
 end
